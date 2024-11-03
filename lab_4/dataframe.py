aaf1a584735f1f9a21aa561bca35b5a250e013cd
catchtimings <chindina.63@gmail.com>
2024-11-03 20:27:29 +0400

    add hist_func and docstring

--- lab_4/dataframe.py
+++ lab_4/dataframe.py
@@ -1,5 +1,6 @@
 import cv2
 import pandas as pd
+from matplotlib import pyplot as plt
 
 def create_dataframe(filename:str) -> pd.DataFrame:
     """
@@ -36,6 +37,13 @@ def add_columns_with_size(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def sort_dataframe(df: pd.DataFrame, max_height: int, max_width:int) -> pd.DataFrame:
+    """
+    The func creates new dataframe and adds in it sorted rows that suit by max values
+    :param df: dataframe
+    :param max_height: max height value
+    :param max_width: max width value
+    :return: new sorted dataframe
+    """
     lst = list()
     for i in range(len(df)):
         if (df.at[i, 'Height'] <= max_height) and (df.at[i, 'Width'] <= max_width):
@@ -45,10 +53,37 @@ def sort_dataframe(df: pd.DataFrame, max_height: int, max_width:int) -> pd.DataF
 
 
 def add_square_column(df:pd.DataFrame) -> pd.DataFrame:
+    """
+    The func adds new column 'square' and calcs height * width
+    :param df: dataframe
+    :return: dataframe with new column
+    """
     df['Square'] = df['Height'] * df['Width']
     return df
 
 
 def sort_by_square(df:pd.DataFrame) -> pd.DataFrame:
-    df = df.sort_values(by='Square')
-    return df
+    """
+    The func copies dataframe and sort it by square from min to max
+    :param df: dataframe
+    :return: new dataframe
+    """
+    sorted_dataframe = df.copy()
+    sorted_dataframe = sorted_dataframe.sort_values(by='Square')
+    return sorted_dataframe
+
+
+def create_hist(df:pd.DataFrame) -> None:
+    """
+    The func creates histogram for dataframe by square
+    :param df: dataframe
+    :return: None
+    """
+    plt.figure(figsize=(10,5))
+
+    df['Square'].hist()
+
+    plt.xlabel('Square')
+    plt.ylabel('Count')
+
+    plt.show()
