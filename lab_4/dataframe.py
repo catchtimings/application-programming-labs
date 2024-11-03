5c308bf72d283464289e22dbbc2611b93671bfdd
catchtimings <chindina.63@gmail.com>
2024-11-03 19:17:47 +0400

    add function that adds columns with size of image

--- lab_4/dataframe.py
+++ lab_4/dataframe.py
@@ -1,3 +1,4 @@
+import cv2
 import pandas as pd
 
 
@@ -10,3 +11,25 @@ def create_dataframe(filename:str) -> pd.DataFrame:
     df = pd.read_csv(filename)
     return df
 
+
+def add_columns_with_size(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    The func adds 3 columns with image height, width and channels
+    :param df: dataframe
+    :return: dataframe with new columns
+    """
+    height = list()
+    width = list()
+    channels = list()
+
+    for item in df['Absolute path']:
+        img = cv2.imread(item)
+        h, w, c = img.shape
+        height.append(h)
+        width.append(w)
+        channels.append(c)
+
+    df['Height'] = height
+    df['Width'] = width
+    df['Channels'] = channels
+    return df
