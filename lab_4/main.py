12356e4d9b8ce9966bf2e5e0991d7262e73e78aa
catchtimings <chindina.63@gmail.com>
2024-11-03 19:52:09 +0400

    update main.py

--- lab_4/main.py
+++ lab_4/main.py
@@ -5,7 +5,8 @@ FILENAME = 'annotation.csv'
 def main():
     df = create_dataframe(FILENAME)
     df2 = add_columns_with_size(df)
-    df2.to_csv('an.csv')
+    s = sort_dataframe(df2, 700, 700)
+    s.to_csv('an.csv')
 
 if __name__ == '__main__':
     main()
