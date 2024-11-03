58f9d863405b0656062b29abf4897d74abbad558
catchtimings <chindina.63@gmail.com>
2024-11-03 20:05:27 +0400

    make 'add_square_column' func more easier

--- lab_4/dataframe.py
+++ lab_4/dataframe.py
@@ -36,17 +36,19 @@ def add_columns_with_size(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def sort_dataframe(df: pd.DataFrame, max_height: int, max_width:int) -> pd.DataFrame:
-    suitable = list()
+    lst = list()
     for i in range(len(df)):
         if (df.at[i, 'Height'] <= max_height) and (df.at[i, 'Width'] <= max_width):
-            suitable.append(df.iloc[i])
-    sorted_dataframe = pd.DataFrame(suitable)
+            lst.append(df.iloc[i])
+    sorted_dataframe = pd.DataFrame(lst)
     return sorted_dataframe
 
 
 def add_square_column(df:pd.DataFrame) -> pd.DataFrame:
-    square = list()
-    for i in range(len(df)):
-        square.append(df.at[i, 'Height'] * df.at[i, 'Width'])
-    df['Square of Height and Width'] = square
+    df['Square'] = df['Height'] * df['Width']
+    return df
+
+
+def sort_by_square(df:pd.DataFrame) -> pd.DataFrame:
+    df = df.sort_values(by='Square')
     return df
