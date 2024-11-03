33549f4b40dad37f684a63c5b452c0337f522d36
catchtimings <chindina.63@gmail.com>
2024-11-03 20:05:48 +0400

    update

--- lab_4/main.py
+++ lab_4/main.py
@@ -7,7 +7,9 @@ def main():
     df2 = add_columns_with_size(df)
     #s = sort_dataframe(df2, 700, 700)
     df2 = add_square_column(df2)
-    df2.to_csv('an.csv')
+    df3 = sort_by_square(df2)
+    df3.to_csv('an.csv')
+
 
 if __name__ == '__main__':
     main()
