8bdf0a44b1bbc9fedf84fe217c537f55cf6a34ec
catchtimings <chindina.63@gmail.com>
2024-11-03 20:31:16 +0400

    update main.py

--- lab_4/main.py
+++ lab_4/main.py
@@ -1,14 +1,33 @@
-from dataframe import *
+from adodbapi.examples.xls_read import filename
 
-FILENAME = 'annotation.csv'
+from dataframe import *
+from parser import get_arguments
+
+MAX_HEIGHT = 700
+MAX_WIDTH = 700
+
 
 def main():
-    df = create_dataframe(FILENAME)
-    df2 = add_columns_with_size(df)
-    #s = sort_dataframe(df2, 700, 700)
-    df2 = add_square_column(df2)
-    df3 = sort_by_square(df2)
-    df3.to_csv('an.csv')
+    try:
+        filename = get_arguments()
+        df = create_dataframe(filename)
+        df.to_csv('df.csv')
+
+        df2 = add_columns_with_size(df)
+        df2.to_csv('df2.csv')
+
+        df3 = sort_dataframe(df2, MAX_HEIGHT, MAX_WIDTH)
+        df3.to_csv('df3.csv')
+
+        df4 = add_square_column(df2)
+        df4.to_csv('df4.csv')
+
+        df5 = sort_by_square(df4)
+        df5.to_csv('df5.csv')
+
+        create_hist(df5)
+    except Exception as exc:
+        print(f'Something went wrong: {exc}')
 
 
 if __name__ == '__main__':
